0b0e90a32f7986b804732fa3c685cfe632f43b5a
Chaokai Lai <chaokai.lai@gmail.com>
2022-02-02 20:53:22 +1000

    learn jenkins

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins
+          Learn Jenkins pipeline
         </a>
       </header>
     </div>
